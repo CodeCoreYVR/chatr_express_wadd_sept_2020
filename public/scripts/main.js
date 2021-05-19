e5a86c6ce2414fd2f1879dceb27dc669515f9cb3
helgakern <helgakern@gmail.com>
2021-05-19 11:06:54 -0700

    Created a get request

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -22,5 +22,33 @@ getReq.open(
 //Send the request:
 getReq.send()
 
+// jQuery's AJAX function:
 
+$.ajax({
+    method: 'GET',
+    url: 'https://pokeapi.co/api/v2/pokemon/mr-mime',
+    // If the request is successful this function will be invoked right after.
+    success(data){
+        console.log(data)
+    },
+})
 
+// Chatr-Express routes:
+// GET /messages -> A JSON array of messages
+// POST /messages -> A confirmation(creates a message)
+// PATCH /messages/:id -> A confirmation(edit a message)
+// DELETE /messages/:id -> A confirmation(delete a message)
+
+// GET request
+// Calling 'fetch' with a URL as its argument will make a GET request to that URL.
+fetch('http://localhost:3434/messages')
+//'fetch' returns an object that represents the HTTP response object.
+// You can use the async method '.text()' or '.json()' to parse its body on the response.
+// Dont's forget to return it from the callback.
+    .then(response => response.json())
+    // .then(data => console.log(data))
+    // This is the same as above. We can pass console.table as a callback because the way
+    // 'console.table' is defined. It will log the argument that is passed to it.
+    // If the data is specifically an array of objects, we can use 'console.table'
+    // to output as a table format. 
+    .then(console.table)
